b39938beba67b20c8dad7684879cd233ccc39620
Gianluca Jahn <gianluca.jahn98@gmail.com>
2025-06-27 02:17:17 +0200

    Fix text color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
             key={pathname}
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: 100 }}
             transition={{
               type: "spring",
               stiffness: 100,
